8bbdc72d231c3a400a59e209fd2183e4e65b0efd
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-04 21:36:19 -0300

    metodo de cadastro

--- projeto-api/src/app/curso/curso.service.ts
+++ projeto-api/src/app/curso/curso.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throws } from 'assert';
 import { Observable, map } from 'rxjs';
 
 import { Curso } from './curso';
@@ -23,6 +24,16 @@ export class CursoService {
       map((res) => {
         this.vetor = res['cursos'];
         return this.vetor
+      })
+    )
+  }
+
+  //Cadastrar curso
+  cadastrarCurso(c:Curso): Observable<Curso[]>{
+    return this.http.post(this.url + "cadastrar", {cursos:c}).pipe(
+      map((res) => {
+        this.vetor.push(res['cursos']);
+        return this.vetor;
       })
     )
   }
